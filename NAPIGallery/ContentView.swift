8c245b018ed5fe2278598c5e2d57fb341f78fe5c
Mitch Fisher <mitch@committed-code.com>
2021-03-06 18:34:03 -0500

    Initial Commit

---
+++ NAPIGallery/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  NAPIGallery
+//
+//  Created by Mitch Fisher on 3/6/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ NAPIGallery/NAPIGalleryApp.swift
@@ -0,0 +1,17 @@
+//
+//  NAPIGalleryApp.swift
+//  NAPIGallery
+//
+//  Created by Mitch Fisher on 3/6/21.
+//
+
+import SwiftUI
+
+@main
+struct NAPIGalleryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
